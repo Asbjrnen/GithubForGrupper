8c088ae8e57d9d8eedc305eb9382d0bae3d87db2
chrisHS03 <chso0005@stud.kea.dk>
2024-10-21 09:51:25 +0200

    ye

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("omg!!!!!");
         System.out.println("yallah");
         System.out.println("marshallah");
+        System.out.println("u cant see me!");
     }
 }
