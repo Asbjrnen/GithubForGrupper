aac6ea36ebd2f552e716cacdf523af2c49733dd3
chrisHS03 <chso0005@stud.kea.dk>
2024-10-21 09:27:09 +0200

    ye

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello kittty!");
-        System.out.println("Diddy Do it or Diddy not?");
+        System.out.println("Diddy Do it or Diddy not?"+ " Yes he did");
+        System.out.println("Hello mr. sus");
     }
 }
